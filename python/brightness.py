bdd6d324fc73635020b40a6c1b741549376ba14f
LukasLejdar <lejdosl@gmail.com>
2023-07-12 18:07:16 +0200

    brightness bins

---
+++ python/brightness.py
@@ -0,0 +1,105 @@
+import sys
+import difflib
+import os
+import subprocess
+import numpy
+import argparse
+import math
+import sys
+from configparser import ConfigParser
+
+CONFIG_FILE_LENGTH=3
+
+# Parse arguments --------------------------
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('--inc', '-i', type=float)
+parser.add_argument('-D', help="Displays")
+parser.add_argument('-s', help="set Brightness", type=float)
+parser.add_argument('-r', help="toggle red shift", action='store_true')
+
+args=parser.parse_args()
+
+# Parse config --------------------------
+
+f = open('/home/lukas/python/brightness_config', 'a+')
+f.seek(0)
+lines = f.readlines()
+lines = lines[:CONFIG_FILE_LENGTH] + ['']*(CONFIG_FILE_LENGTH-len(lines))
+
+print(lines)
+
+isAlreadyRunning = False
+hasRedshift = False
+lastDisplayedPromt = ''
+
+try:
+    isAlreadyRunning = lines[0].lower() in ['true', 'true\n', '1']
+
+    lastDisplayedPromt = lines[1]
+    hasRedshift = lines[2].lower() in ['true', 'true\n', '1']
+except:
+    print('Reading config failed')
+
+lines[0]='True'
+lines[1]=str(args.D or '')
+
+print(hasRedshift)
+f.truncate(0)
+f.write('\n'.join(lines))
+
+# Det Display info --------------------------
+
+out = subprocess.check_output("xrandr | grep ' connected' | awk '{print $1}'", shell=True)
+displays = out.decode('utf-8').split('\n')
+displays.pop()
+
+out = subprocess.check_output("xrandr --verbose | grep 'Brightness' | awk '{print $2}'", shell=True)
+brightness = out.decode('utf-8').split('\n')
+brightness.pop()
+
+# Handle brightness --------------------------
+
+def resetRedShiftOnDisplay(i):
+    print(f"reset {displays[i]} to {brightness[i]}")
+    os.system(f"xrandr --output {displays[i]} --brightness {float(brightness[i])}")
+
+def applyChange(i):
+    newBr = float(brightness[i])
+
+    if (args.s != None):
+        newBr = args.s
+    elif(args.inc != None):
+        frac = newBr/2+0.5
+        print(frac)
+        newBr = numpy.clip(newBr+args.inc*frac, 0.1, 1)
+
+    os.system(f"xrandr --output {displays[i]} --brightness {newBr}")
+    print(f"changed {displays[i]} to {newBr}")
+
+if(args.D != None):
+    toApply = args.D.split(',')
+    [applyChange(int(k)) for k in toApply]
+
+    #s = set(toApply)
+    #displaysLeft = [x for x in range(len(displays)) if x not in s]
+    #[resetRedShiftOnDisplay(k) for k in displaysLeft]
+else:
+    [applyChange(k) for k in range(len(displays))]
+
+# Handle redshift --------------------------
+
+
+if(args.r):
+    hasRedshift = not hasRedshift
+    if (hasRedshift):
+        os.system("redshift -O 4000")
+    
+    lines[2]=str(hasRedshift)
+
+lines[0]='False'
+f.truncate(0)
+f.write('\n'.join(lines))
+f.close()
+
