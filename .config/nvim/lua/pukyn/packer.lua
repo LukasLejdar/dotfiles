f6db8cdb01cbad129444bffa6859c50eba146dd2
Lukáš Lejdar <lejdosl@gmail.com>
2023-11-03 18:54:32 +0100

    telescope to fzf

--- .config/nvim/lua/pukyn/packer.lua
+++ .config/nvim/lua/pukyn/packer.lua
@@ -7,13 +7,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-
     use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use ('theprimeagen/harpoon')
     use ('tpope/vim-fugitive')
@@ -21,7 +14,8 @@ return require('packer').startup(function(use)
     use ('SirVer/ultisnips')
     use ('KeitaNakamura/tex-conceal.vim')
     use ('xiyaowong/transparent.nvim')
-
+    use { 'ibhagwan/fzf-lua'}
+    use { 'nvim-lua/plenary.nvim' }
     use({ 'rose-pine/neovim', as = 'rose-pine' })
     vim.cmd('colorscheme rose-pine')
 
