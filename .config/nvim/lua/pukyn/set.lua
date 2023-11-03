f705f094ac2e3b566cdceee910cc40687323438a
Lukáš Lejdar <lejdosl@gmail.com>
2023-11-03 19:07:47 +0100

    no wrap

--- .config/nvim/lua/pukyn/set.lua
+++ .config/nvim/lua/pukyn/set.lua
@@ -7,8 +7,10 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-
-vim.opt.wrap = true
+vim.o.smartcase = true
+vim.opt.wrap = false
+vim.opt.colorcolumn = "80"
+vim.opt.updatetime = 50
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -16,8 +18,3 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
-vim.o.smartcase = true
